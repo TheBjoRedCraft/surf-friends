2832c994ef27e903c4af43d165f7b7ffb63c2853
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-03-24 14:53:02 +0100

    feat: Add utility function for editing FriendData and updating database

---
+++ surf-friends-velocity/src/main/kotlin/dev/slne/surf/friends/velocity/util/frienduser-util.kt
@@ -0,0 +1,10 @@
+package dev.slne.surf.friends.velocity.util
+
+import dev.slne.surf.friends.api.data.FriendData
+import dev.slne.surf.friends.api.user.FriendUser
+import dev.slne.surf.friends.core.databaseService
+
+fun FriendData.edit(block: FriendData.() -> Unit) {
+    this.block()
+    databaseService.updateData(this)
+}
