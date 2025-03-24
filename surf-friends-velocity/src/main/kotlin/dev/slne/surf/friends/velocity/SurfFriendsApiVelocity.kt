d72da87c68f1a271210bb23f9b63976f0ca2f1f2
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-03-24 13:45:59 +0100

    feat: Implement friend request and management functionalities in SurfFriendsApiVelocity

--- surf-friends-velocity/src/main/kotlin/dev/slne/surf/friends/velocity/SurfFriendsApiVelocity.kt
+++ surf-friends-velocity/src/main/kotlin/dev/slne/surf/friends/velocity/SurfFriendsApiVelocity.kt
@@ -4,41 +4,42 @@ import com.google.auto.service.AutoService
 
 import dev.slne.surf.friends.api.SurfFriendsApi
 import dev.slne.surf.friends.api.user.FriendUser
+import dev.slne.surf.friends.core.friendService
+import it.unimi.dsi.fastutil.objects.ObjectSet
 
 import net.kyori.adventure.util.Services.Fallback
 
 @AutoService(SurfFriendsApi::class)
 class SurfFriendsApiVelocity(): SurfFriendsApi, Fallback {
     override suspend fun sendFriendRequest(sender: FriendUser, target: FriendUser) {
-        
+        friendService.sendFriendRequest(sender, target)
     }
 
     override suspend fun acceptFriendRequest(sender: FriendUser, target: FriendUser) {
-        TODO("Not yet implemented")
+        friendService.acceptFriendRequest(sender, target)
     }
 
     override suspend fun declineFriendRequest(sender: FriendUser, target: FriendUser) {
-        TODO("Not yet implemented")
+        friendService.declineFriendRequest(sender, target)
     }
 
     override suspend fun addFriend(player: FriendUser, friend: FriendUser) {
-        TODO("Not yet implemented")
+        friendService.addFriend(player, friend)
     }
 
     override suspend fun removeFriend(player: FriendUser, friend: FriendUser) {
-        TODO("Not yet implemented")
+        friendService.removeFriend(player, friend)
     }
 
-    override suspend fun getFriends(player: FriendUser) {
-        TODO("Not yet implemented")
+    override suspend fun getFriends(player: FriendUser): ObjectSet<FriendUser> {
+        return friendService.getFriends(player)
     }
 
-    override suspend fun toggleAnnouncements(player: FriendUser) {
-        TODO("Not yet implemented")
+    override suspend fun toggleAnnouncements(player: FriendUser): Boolean {
+        return friendService.toggleAnnouncements(player)
     }
 
-    override suspend fun toggleAnnouncementSounds(player: FriendUser) {
-        TODO("Not yet implemented")
+    override suspend fun toggleAnnouncementSounds(player: FriendUser): Boolean {
+        return friendService.toggleAnnouncementSounds(player)
     }
-
 }
