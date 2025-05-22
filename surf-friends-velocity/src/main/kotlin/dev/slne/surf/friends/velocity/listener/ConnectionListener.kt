3f93fab17f22eb54455456f3c86660a2ff2e8b4b
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-05-22 20:16:20 +0200

    feat: Notify players of online friends in connection listener

--- surf-friends-velocity/src/main/kotlin/dev/slne/surf/friends/velocity/listener/ConnectionListener.kt
+++ surf-friends-velocity/src/main/kotlin/dev/slne/surf/friends/velocity/listener/ConnectionListener.kt
@@ -5,6 +5,8 @@ import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.player.PlayerChooseInitialServerEvent
 import dev.slne.surf.friends.core.service.friendService
 import dev.slne.surf.friends.velocity.container
+import dev.slne.surf.friends.velocity.plugin
+import dev.slne.surf.friends.velocity.util.getOnlineFriends
 import dev.slne.surf.friends.velocity.util.sendText
 import dev.slne.surf.surfapi.core.api.font.toSmallCaps
 import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
@@ -17,6 +19,23 @@ class ConnectionListener {
 
         container.launch {
             val friendRequests = friendService.getReceivedFriendRequests(player.uniqueId)
+            val onlineFriends = friendService.getOnlineFriends(player.uniqueId)
+
+            if(onlineFriends.isNotEmpty()) {
+                player.uniqueId.sendText {
+                    info("Aktuell sind ")
+                    variableValue(onlineFriends.size)
+                    info(" deiner Freunde online. ")
+
+                    append {
+                        clickRunsCommand("/friend list")
+                        info("[Ansehen]".toSmallCaps())
+                        hoverEvent(buildText {
+                            info("Klicke hier, um die Freunde anzusehen.")
+                        })
+                    }
+                }
+            }
 
             if(friendRequests.isNotEmpty()) {
                 player.uniqueId.sendText {
